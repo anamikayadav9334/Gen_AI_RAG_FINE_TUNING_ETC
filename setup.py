b00d0e7378af15fae821d6bbaf04ffa12748b921
Anamika Yadav <anamikayadav01234@gmail.com>
2026-05-13 21:39:02 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'QApplication',
+    version= '0.0.1',
+    author= 'sunny savita',
+    author_email= 'yellow.blue9334@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
